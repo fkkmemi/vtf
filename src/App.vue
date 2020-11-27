c43b1a862fdce61f137c6f2f53a09ea910471151
fkkmemi <fkkmemi@gmail.com>
2020-11-27 20:32:00 +0900

    vuetify installed

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <v-btn>iiii</v-btn>
     </div>
     <router-view/>
   </div>
